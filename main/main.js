df8ae464437c15a95ef2ced97e819a5f26ed7211
Seact <seact_l@outlook.com>
2019-05-31 22:17:01 +0800

    ''

--- main/main.js
+++ main/main.js
@@ -1,4 +1,16 @@
 module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+  let nums = arguments[0].split('');
+  let chars_set = [
+    ['._.', '...', '._.', '._.', '...', '._.', '._.', '._.', '._.', '._.'],
+    ['|.|', '..|', '._|', '._|', '|_|', '|_.', '|_.', '..|', '|_|', '|_|'],
+    ['|_|', '..|', '|_.', '._|', '..|', '._|', '|_|', '..|', '|_|', '..|']
+  ]
+  return [
+    nums.map(c => chars_set[0][c]).join(' '),
+    '\n',
+    nums.map(c => chars_set[1][c]).join(' '),
+    '\n',
+    nums.map(c => chars_set[2][c]).join(' '),
+    '\n'
+  ].join('');
 };
